89bf79bd17ba9ffa6bd1d38e7c7986f535f941f1
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-08-01 10:50:25 -0500

    Added close button, white jpanels and change extended state to maximized both

--- src/client/gui/Window.java
+++ src/client/gui/Window.java
@@ -2,7 +2,12 @@ package client.gui;
 
 import client.logic.Game;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
@@ -10,18 +15,47 @@ import javax.swing.JFrame;
  */
 public class Window extends JFrame{
     
+    private int WIDTH, HEIGHT;
+    
     public Window(Game game){
+        WIDTH = 1360;
+        HEIGHT = 768;
+
+        addJButtons();
         drawBackground();
         
-        setSize(1280, 720);
-        setLocationRelativeTo(this);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLayout(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setUndecorated(true);
         setVisible(true);
     }
     
     private void drawBackground(){
-        getContentPane().setBackground(new Color(0, 0, 0));
+        getContentPane().setBackground(Color.WHITE);
+        
+        JPanel left = new JPanel();
+        left.setBounds(32, 32, (WIDTH-96)/2, HEIGHT-64);
+        left.setBackground(Color.BLACK);
+        add(left);
+        
+        JPanel right = new JPanel();
+        right.setBounds(WIDTH/2 +16, 32, (WIDTH-96)/2, HEIGHT-64);
+        right.setBackground(Color.BLACK);
+        add(right);
+    }
+
+    private void addJButtons() {
+        JButton close = new JButton("X");
+        close.setBounds(WIDTH-60, 10, 50, 32);
+        close.setBackground(Color.RED);
+        close.setFocusable(false);
+        close.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        add(close);
     }
     
 }
